f2b17f9d7e35e2e49376a2e65a12bba20ddd5cf9
akhatar <o.akhatar5927@gmail.com>
2023-07-27 16:29:05 +0100

    app

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-5xl font-bold mb-3">Todo Tamtam</h1>
+        <h1 className="text-5xl font-bold mb-3">Todo.Tamtam</h1>
         <AddTask />
       </div>
       <TodoList tasks={tasks} />
