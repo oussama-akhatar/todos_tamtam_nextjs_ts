d602c68689196aa62131020c32846343145681a6
akhatar <o.akhatar5927@gmail.com>
2023-07-28 11:50:26 +0100

    some generals edits

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,7 +1,7 @@
 import { ClipLoader } from 'react-spinners';
 
 const Loader = ({size} : any) => {
-    return <ClipLoader size={size} />
+    return <span className="loading loading-spinner loading-md"></span>
 };
 
 export default Loader;
