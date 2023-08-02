8f771ebdda855b9cd7bdd9ce24a906bc099a2e9d
akhatar <o.akhatar5927@gmail.com>
2023-08-02 15:40:18 +0100

    styling some components

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -2,7 +2,7 @@
 import Modal from './Modal'
 import { FormEventHandler, useState } from 'react'
 import { addTodo } from '@/api'
-import {MdAdd} from 'react-icons/md'
+import { Button } from '@material-tailwind/react'
 
 const AddTask = ({ updateTasks, setIsLoading }: any) => {
   const [modalOpen, setModalOpen] = useState<boolean>(false)
@@ -30,7 +30,12 @@ const AddTask = ({ updateTasks, setIsLoading }: any) => {
 
   return (
     <>
-      <button onClick={() => { setModalOpen(true) }} className="btn text-white bg-blue-600 hover:bg-blue-700 text-xl"><MdAdd /></button>
+      <Button onClick={() => { setModalOpen(true) }} size='sm' className='flex items-center gap-3' variant="gradient">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>Add Task</span>
+      </Button>
       <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} >
         <form onSubmit={handleSubmitNewTodo}>
           <h3 className='font-bold text-lg'>Add new task</h3>
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -112,17 +112,14 @@ const Task = ({ task, deleteTask, updateTask }: TodoProps) => {
                         ) : (
                             <>
                                 {task.is_done ? (
-                                    <AiOutlineCheckCircle
-                                        onClick={handleToggleDone}
-                                        cursor="pointer"
-                                        className="text-green-600 lg:text-2xl"
-                                    />
+                                    <svg onClick={handleToggleDone} cursor="pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-green-600 w-8 h-8">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
                                 ) : (
-                                    <AiOutlineCloseCircle
-                                        onClick={handleToggleDone}
-                                        cursor="pointer"
-                                        className="text-red-600 lg:text-2xl"
-                                    />
+                                    <svg onClick={handleToggleDone} cursor="pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-red-600 w-8 h-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
                                 )}
                             </>
                         )}
@@ -130,7 +127,12 @@ const Task = ({ task, deleteTask, updateTask }: TodoProps) => {
                     <td className={`flex gap-5 items-center p-8 ${task.is_deleted ? "justify-end" : "justify-center"}`}>
                         {
                             !task.is_deleted
-                                ? <FiEdit onClick={() => setOpenModalEdit(true)} cursor="pointer" className='text-blue-500 lg:text-2xl' />
+                                // ? <FiEdit onClick={() => setOpenModalEdit(true)} cursor="pointer" className='text-blue-500 lg:text-2xl' />
+                                ? (
+                                    <svg onClick={() => setOpenModalEdit(true)} cursor="pointer"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-blue-500 w-7 h-7">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>
+                                )
                                 : null
                         }
 
@@ -146,8 +148,19 @@ const Task = ({ task, deleteTask, updateTask }: TodoProps) => {
                         </Modal>
                         {
                             !task.is_deleted
-                                ? <FiTrash2 onClick={() => setOpenModalDelete(true)} cursor="pointer" className='text-red-600 lg:text-2xl' />
-                                : <BiUndo onClick={() => handleUndoDeleteTodo()} cursor="pointer" className='text-green-600 lg:text-2xl self-end' />
+                                ? /*<FiTrash2 onClick={() => setOpenModalDelete(true)} cursor="pointer" className='text-red-600 lg:text-2xl' />*/
+                                (
+                                    <svg onClick={() => setOpenModalDelete(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" cursor="pointer" className="w-7 h-7 text-red-600">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                    </svg>
+
+                                )
+                                // : <BiUndo onClick={() => handleUndoDeleteTodo()} cursor="pointer" className='text-green-600 lg:text-2xl self-end' />
+                                : (
+                                    <svg onClick={() => handleUndoDeleteTodo()} cursor="pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-green-600 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                                    </svg>
+                                )
                         }
 
                         <Modal modalOpen={openModalDelete} setModalOpen={setOpenModalDelete} >
